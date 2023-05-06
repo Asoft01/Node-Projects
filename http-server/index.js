611737de6ec4fd0c66d1670995ee27c49427bdee
Adeleke Hammed Lekan <lekhad19@gmail.com>
2023-05-06 19:06:41 -0400

    Parameter URLs modified

--- http-server/index.js
+++ http-server/index.js
@@ -21,19 +21,45 @@ const friends = [
 server.on('request', (req, res) => {
     const items = req.url.split('/');
     // /friends/2  => ['', 'friends', '2']
-    if(req.url === '/friends' ){
-        // res.writeHead(200, {
-        //     // 'Content-Type' : 'text/plain', 
-        //     'Content-Type' : 'application/json'
-        // });
+    // if(req.url === '/friends' ){
+    //     // res.writeHead(200, {
+    //     //     // 'Content-Type' : 'text/plain', 
+    //     //     'Content-Type' : 'application/json'
+    //     // });
+    //     res.statusCode = 200; 
+    //     res.setHeader('Content-Type', 'application/json');
+    //     // res.end('Hello! Sir Isaac Newton is your friend!');
+    //     res.end(JSON.stringify({
+    //         id : 1,
+    //         name : 'Sir Isaac Newton'
+    //     }));
+    // }else if(req.url === '/messages'){
+    //     res.setHeader('Content-Type', 'text/html');
+    //     res.write('<html>');
+    //     res.write('<body>');
+    //     res.write('<ul>');
+    //     res.write('<li>Hello Isaac!</li>');
+    //     res.write('<li>What are your thoughts on astronomy?');
+    //     res.write('</ul>');
+    //     res.write('</body>');
+    //     res.write('</html>');
+    //     res.end();
+    // }else {
+    //     res.statusCode = 404;
+    //     res.end();
+        
+    // }
+
+    if(items[1] === 'friends'){
         res.statusCode = 200; 
         res.setHeader('Content-Type', 'application/json');
-        // res.end('Hello! Sir Isaac Newton is your friend!');
-        res.end(JSON.stringify({
-            id : 1,
-            name : 'Sir Isaac Newton'
-        }));
-    }else if(req.url === '/messages'){
+        if(items.length === 3) {
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+        }else{
+            res.end(JSON.stringify({friends}));
+        }
+    }else if(items[1] === 'messages'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
