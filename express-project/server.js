f042507eb2d1943cc8f1ca5462d5da3624f34500
Adeleke Hammed Lekan <lekhad19@gmail.com>
2023-05-08 06:49:11 -0400

    Express introduction

--- express-project/server.js
+++ express-project/server.js
@@ -4,8 +4,38 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) =>  {
-    res.send('Hellloooooooooo')
+/////////////////////////////////////////////////////////////////
+
+// app.get('/', (req, res) =>  {
+//     res.send('Hellloooooooooo')
+// });
+
+// app.get('/message', (req, res) => {
+//     // res.send('<ul><li> Hello Albert!</li></ul>');
+//     res.send({
+//         id : 1, 
+//         name : 'Sir Isaac Newton'
+//     });
+// }); 
+
+// app.post('/message', (req, res) => {
+//     console.log('Updating Messages...');
+// }) 
+
+//////////////////////////////////////////////////////////////////
+const friends = [
+    {
+        id : 0, 
+        name : 'Albert Einsten'
+    }, 
+    {
+        id : 1, 
+        name : 'Sir Isaac Newton'
+    }
+]; 
+
+app.get('/friends', (req, res) =>  {
+    res.send(friends);
 });
 
 app.get('/message', (req, res) => {
@@ -19,6 +49,7 @@ app.get('/message', (req, res) => {
 app.post('/message', (req, res) => {
     console.log('Updating Messages...');
 }) 
+
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`);
 });
