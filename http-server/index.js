415ec5b532bf295b415c7bd205c942933c70f662
Adeleke Hammed Lekan <lekhad19@gmail.com>
2023-05-08 03:19:56 -0400

    POSTING DATA to the Server

--- http-server/index.js
+++ http-server/index.js
@@ -50,7 +50,44 @@ server.on('request', (req, res) => {
         
     // }
 
-    if(items[1] === 'friends'){
+/////////////////////////////////////////////////////////////////////////////////////////////
+    // if(items[1] === 'friends'){
+    //     res.statusCode = 200; 
+    //     res.setHeader('Content-Type', 'application/json');
+    //     if(items.length === 3) {
+    //         const friendIndex = Number(items[2]);
+    //         res.end(JSON.stringify(friends[friendIndex]));
+    //     }else{
+    //         res.end(JSON.stringify({friends}));
+    //     }
+    // }else if(items[1] === 'messages'){
+    //     res.setHeader('Content-Type', 'text/html');
+    //     res.write('<html>');
+    //     res.write('<body>');
+    //     res.write('<ul>');
+    //     res.write('<li>Hello Isaac!</li>');
+    //     res.write('<li>What are your thoughts on astronomy?');
+    //     res.write('</ul>');
+    //     res.write('</body>');
+    //     res.write('</html>');
+    //     res.end();
+    // }else {
+    //     res.statusCode = 404;
+    //     res.end();
+    // }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// fetch('http://localhost:3000/friends', {
+//     method: 'POST', 
+//     body: JSON.stringify({ id: 3, name: 'Ryan Dahl'})
+// });
+if(req.method === 'POST' && items[1] === 'friends'){
+    req.on('data', (data) => {
+        const friend = data.toString();
+        console.log('Request:', friend);
+        friends.push(JSON.parse(friend));
+    });
+}else if(req.method === 'GET' && items[1] === 'friends'){
         res.statusCode = 200; 
         res.setHeader('Content-Type', 'application/json');
         if(items.length === 3) {
@@ -59,7 +96,7 @@ server.on('request', (req, res) => {
         }else{
             res.end(JSON.stringify({friends}));
         }
-    }else if(items[1] === 'messages'){
+    }else if(req.method === 'GET' && items[1] === 'messages'){
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
@@ -73,7 +110,6 @@ server.on('request', (req, res) => {
     }else {
         res.statusCode = 404;
         res.end();
-        
     }
 });
 
