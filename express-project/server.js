8436a686fa53e1f5abac7c1235ca317933eca2c4
Adeleke Hammed <lekhad19@gmail.com>
2023-05-11 10:41:45 -0400

    Serving Websites With Node

--- express-project/server.js
+++ express-project/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const friendsRouter = require('./routes/friends.router');
 // const friendsController = require('./controllers/friends.controller');
@@ -48,7 +49,8 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
-app.use('/site', express.static('public'));
+// app.use('/site', express.static('public'));
+app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 // app.post('/friends', (req, res) => {
