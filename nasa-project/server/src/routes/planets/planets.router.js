7a1fa8d15e6963d9c186c28a1ae8431381a90fd0
Adeleke Hammed <lekhad19@gmail.com>
2023-05-23 19:40:25 -0400

    Getting Planets

--- nasa-project/server/src/routes/planets/planets.router.js
+++ nasa-project/server/src/routes/planets/planets.router.js
@@ -1,5 +1,10 @@
 const express = require('express'); 
 
+// const planetsController = require('./planets.controller');
+const {
+    getAllPlanets, 
+} = require('./planets.controller');
+
 const planetsRouter = express.Router(); 
 
 planetsRouter.get('/planets', getAllPlanets); 
