6cafec5b3a4daaecc59a2050984656045d45b733
Adeleke Hammed Lekan <lekhad19@gmail.com>
2024-02-20 23:23:20 +0100

    added strings to the graph ql

--- graphql/server.js
+++ graphql/server.js
@@ -1,8 +1,26 @@
 const express = require('express')
-const { expressGraphQL } = require('express-graphql')
+const { graphqlHTTP  } = require('express-graphql')
+const { 
+    GraphQLSchema, 
+    GraphQLObjectType, 
+    GraphQLString
+} = require('graphql');
 const app = express()
 
-app.use('/graphql', expressGraphQL({
+const schema = new GraphQLSchema({
+    query : new GraphQLObjectType({
+        name : 'HelloWorld', 
+        fields: () => ({
+            message : { 
+                type : GraphQLString,  
+                resolve: () => 'Hello World'
+            }
+        }) 
+    })
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema : schema, 
     graphiql: true
 }))
 
