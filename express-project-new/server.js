7e9b04c94fb264356730f2bb6561b66e3bbe6883
Adeleke Hammed <lekhad19@gmail.com>
2024-07-30 11:38:35 +0100

    express project new

--- express-project-new/server.js
+++ express-project-new/server.js
@@ -13,7 +13,32 @@ const friends = [
         id: 1, 
         name: "Sir Isaac Newton"
     }
-]
+]; 
+
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    // console.log(`${req.method} ${req.url}`); 
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+    if(!req.body.name) {
+       return res.status(400).json({
+            error: 'Missing friend name'
+        });
+    }
+    const newFriend = {
+        name: req.body.name, 
+        id: friends.length
+    };
+    friends.push(newFriend);
+
+    res.json(newFriend);
+});
 
 app.get('/', (req, res) => {
     res.send('Hellooooo');
