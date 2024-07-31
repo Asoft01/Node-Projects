009f1d8ce9c09f82228cf2f98142be5e823b6dd1
Adeleke Hammed <lekhad19@gmail.com>
2024-07-31 22:23:33 +0100

    exclude modules

---
+++ graphql-hello-world/src/index.js
@@ -0,0 +1,38 @@
+/////////////// Initialization ////////////////////////////////////
+
+// const {ApolloServer, gql} = require('apollo-server'); 
+
+// const typeDefs = gql`
+//  type Query {
+//     hello: String!
+//  }
+// `; 
+
+// const resolvers = {
+//     Query: {
+//         hello: () => 'hello world'
+//     }
+// }
+
+// const server = new ApolloServer({ typeDefs, resolvers }); 
+
+// server.listen().then(({ url }) => console.log(`server started at ${url}`));
+
+/////////////////////////////////////////////////////////////////////
+const {ApolloServer, gql} = require('apollo-server'); 
+
+const typeDefs = gql`
+ type Query {
+    hello: String!
+ }
+`; 
+
+const resolvers = {
+    Query: {
+        hello: () => 'hello world'
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers }); 
+
+server.listen().then(({ url }) => console.log(`server started at ${url}`));
--- ytb/app.js
+++ ytb/app.js
@@ -3,8 +3,13 @@
 
 const names = require('./4-names');
 const sayHi = require('./5-utils');
-console.log(names);
+const data = require('./6-alternative-flavour'); 
+require('./7-mind-grenade');
+// console.log(names);
+console.log(data);
 
-sayHi('susan'); 
-sayHi(john); 
-sayHi(peter);
+// sayHi('susan'); 
+// // sayHi(john); 
+// // sayHi(peter);
+// sayHi(names.john); 
+// sayHi(names.peter);
